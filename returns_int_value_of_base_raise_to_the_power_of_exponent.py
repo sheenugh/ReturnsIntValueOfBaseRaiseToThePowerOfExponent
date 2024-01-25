d6c03e9899fb14b3dcb2651c92ee0baa89e036eb
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 03:37:26 +0800

    done case 2

--- returns_int_value_of_base_raise_to_the_power_of_exponent.py
+++ returns_int_value_of_base_raise_to_the_power_of_exponent.py
@@ -10,7 +10,16 @@ def case1_exponent(base,exp):
         number = number - 1
     print(base, "raises to the power of", exp, "is:", result, "i.e", "(2 * 2 * 2 * 2 * 2 = 32)")
 
+def case2_exponent(base,exp):
+    number = exp
+    result = 1
+    while number > 0:
+        result = result * base
+        number = number - 1
+    print(base, "raises to the power of", exp, "is:", result, "i.e", "(5 * 5 * 5 * 5 = 625)")
+
 # >>>>>>>>> PSEUDO CODE <<<<<<<<<
+# ----- ACTUAL CODES -----
 # - Given: Case 1
 base1 = 2
 exponent1 = 5 
@@ -21,4 +30,5 @@ exponent2 = 4
 
 # - Caller of the def function/s
 case1_exponent(base1, exponent1)
-# ----- ACTUAL CODES -----
+case2_exponent(base2, exponent2)
+
