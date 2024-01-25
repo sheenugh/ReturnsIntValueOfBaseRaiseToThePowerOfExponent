7341f6b485d00bb71d5308b9bcb31ee88f08a36c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 03:25:11 +0800

    adding given

--- returns_int_value_of_base_raise_to_the_power_of_exponent.py
+++ returns_int_value_of_base_raise_to_the_power_of_exponent.py
@@ -3,8 +3,15 @@
 
 # ----- FUNCTIONS ------
 
+
 # >>>>>>>>> PSEUDO CODE <<<<<<<<<
-# - Given
+# - Given: Case 1
+base1 = 2
+exponent1 = 5 
+
+# - Given: Case 2
+base2 = 5
+exponent2 = 4 
 # - Caller of the def function/s
 
 # ----- ACTUAL CODES -----
